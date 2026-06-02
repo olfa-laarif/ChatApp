3749993275e55896f5b46be3e34d47e42bcc852e
denise zahreddine <denise.zahreddine@gmail.com>
2026-06-02 13:58:53 +0200

    add swager + token

---
+++ src/main/java/olfa/laarif/chatapp/controller/UserController.java
@@ -0,0 +1,29 @@
+package olfa.laarif.chatapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/private")
+public class UserController {
+
+    @GetMapping()
+    public String hello(@AuthenticationPrincipal Jwt jwt) {
+
+        return "Hello " + jwt.getClaim("email");
+    }
+
+
+}
+
---
+++ src/main/java/olfa/laarif/chatapp/controller/request/UserRegistrationDto.java
@@ -0,0 +1,10 @@
+package olfa.laarif.chatapp.controller.request;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+    private String username;
+    private String password;
+    private String role;
+}
